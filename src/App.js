35fa1d155122748654838a006fdc7b0d04d3280a
Jeon <jbsgo87@gmail.com>
2020-04-25 06:12:35 +0900

    get the movie information using api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from "prop-types";
+import axios from "axios";
+import Movie from "./Movie";
 //function component
 /*
 function Potato( {fav , image , rating} ){
@@ -10,43 +12,49 @@ function Potato( {fav , image , rating} ){
         </div>;
 }*/
 
+/*    {isLoading ? "Loading...": movies.map(movie => {
+      console.log(movie);
+      return <Movie id={movie.id} year={movie.year} title={movie.title} summary={movie.summary} poster={movie.medium_cover_image} />
+    })} */
 //react class component
 class App extends React.Component{
   state = {
-    count: 0
+     isLoading: true,
+     movies: []
   };
 
-  constructor(){
-    super();
-    console.log("hello");
-  }
-  add = () => {
-    console.log("add");
-  //  this.state.count = 1;
-    this.setState(current => ({count: current.count + 1})); // recall render again
-  };
-  minus=()=>{
-    console.log("minus");
-//    this.state.count = -1;
-    this.setState(current => ({count: current.count - 1}));
-
-  };
-  componentWillUnmount(){
-    console.log("unmount");
-  }
-  componentDidUpdate(){
-    console.log("component updated");
-  }
+  getMovies = async function(){
+     const {data: { data : {movies} } }= await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+     //console.log(movies);
+     this.setState({movies:movies , isLoading: false});
+  } //same as below
+  /*
+  getMovies = async() =>{
+     const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }*/
   componentDidMount(){
-    console.log("component rendered");
+//    setTimeout(  () => { this.setState({isLoading: false});
+//    } , 6000);
+  //   const movies = axios.get("https://yts-proxy.now.sh/list_movies.json")
+    this.getMovies();
   }
   render() {// automatically called
     console.log("render");
+    const { isLoading , movies } = this.state;
     return  (
       <div>
-        <h1>The number is: { this.state.count }</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
+
+
+      {isLoading ? "Loading..."
+                :  movies.map(movie =>
+                    <Movie
+                           key={movie.id}
+                           id={movie.id}
+                           year={movie.year}
+                           title={movie.title}
+                           summary={movie.summary}
+                           poster={movie.medium_cover_image} />
+      )}
       </div>
     );
 
